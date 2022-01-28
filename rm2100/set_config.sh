314fa9a59145d00248c3b940d7410f2a0b0a889a
Retayihtevr <971686021@qq.com>
2022-01-29 00:02:52 +0800

    x

--- rm2100/set_config.sh
+++ rm2100/set_config.sh
@@ -19,4 +19,4 @@ git clone --depth=1 https://github.com/kenzok8/small
 
 cd -
 
-sed -i 's/192.168.1.1/192.168.31.1/g' package/base-files/files/bin/config_generate
+#sed -i 's/192.168.1.1/192.168.31.1/g' package/base-files/files/bin/config_generate
